6b98aacb0d5ae7c5616114aca52cbd2b83968646
System Administrator <admin@example.com>
2019-09-06 18:09:55 +0000

    Changes by 'System Administrator' on 'my app – 1.0.0' -- Build #1

---
+++ sn/my app/Dictionary Entry/x_400965_my_app_table/active_true/number/calculation.js
@@ -0,0 +1,15 @@
+/* 
+ * Application : my app
+ * ClassName   : sys_dictionary
+ * Created On  : 2019-09-02 14:15:04
+ * Created By  : admin
+ * Updated On  : 2019-09-02 14:15:04
+ * Updated By  : admin
+ * URL         : /sys_dictionary.do?sys_id=f52d1bcbdbe7f3006a6ad001cf9619a6
+ */
+(function calculatedFieldValue(current) {
+
+	// Add your code here
+	return '';  // return the calculated value
+
+})(current);
